fdd4bdfeedd77b0c5fd5599923473f85f4833f74
zeenu94 <56841101+zeenu94@users.noreply.github.com>
2025-10-29 18:24:43 -0400

    Create step1_genesis.R

---
+++ scripts/step1_genesis.R
@@ -0,0 +1,14 @@
+library(SeqArray)
+library(SeqVarTools,lib.loc = "/share/pkg.7/r/4.0.5/install/lib64/R/library")
+library(SNPRelate)
+library(GWASTools)
+library(GENESIS)
+
+bed.fn <- "step5.bed"
+bim.fn <- "step5.bim"
+fam.fn <- "step5.fam"
+
+gds <- "step5.gds"
+
+
+snpgdsBED2GDS(bed.fn, fam.fn, bim.fn, gds)
